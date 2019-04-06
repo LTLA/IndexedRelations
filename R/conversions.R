91f7e20e623c62c04676a552d6efad5db91e99a8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-04-06 11:51:18 -0700

    Expanded available conversion methods to/from IR objects.

--- R/conversions.R
+++ R/conversions.R
@@ -1,14 +1,17 @@
-#' Conversion to Pairs
+#' IndexedRelation conversions
 #'
-#' Convert an IndexedRelations object from a Pairs object, and vice versa.
+#' Convert an IndexedRelations object to and from different Bioconductor classes.
 #'
-#' @param x A \linkS4class{IndexedRelations} object with two partners.
-#' @param from A \linkS4class{Pairs} object.
-#' 
-#' @return
-#' \code{makePairsFromIndexedRelations} will return a Pairs object where the first partner in \code{x} is the first vector in the output.
-#' 
-#' The \code{as} method will return an IndexedRelations object where the first vector in \code{from} becomes the first partner in the output.
+#' @details
+#' A number of \code{coerce} methods are available to convert to/from an \linkS4class{IndexedRelations} object:
+#' \itemize{
+#' \item \code{as(from, "IndexedRelations")} for a \linkS4class{Pairs} \code{x} will return an IndexedRelations object where the first vector in \code{from} becomes the first partner in the output.
+#' \item \code{as(from, "IndexedRelations")} for a \linkS4class{DataFrame} \code{x} will return an IndexedRelations object where the first column in \code{from} becomes the first partner in the output.
+#' \item \code{as(from, "DataFrame")} for an IndexedRelations \code{x} will return a DataFrame object where the first column is the first partner in \code{From}.
+#' }
+#'
+#' In addition, \code{makePairsFromIndexedRelations(x)} will create a Pairs instance from an IndexedRelations \code{x}.
+#' This requires that \code{x} only has two partners in each relationship, otherwise an error is thrown.
 #'
 #' @author Aaron Lun
 #' @examples
@@ -20,6 +23,19 @@
 #' back <- makePairsFromIndexedRelations(ir)
 #' back
 #'
+#' df <- as(ir, "DataFrame")
+#' df
+#' 
+#' as(df, "IndexedRelations")
+#' 
+#'
+#' @docType methods 
+#' @name IR conversions
+#' @rdname conversions
+#' @aliases makePairsFromIndexedRelations
+#' @aliases coerce,Pairs,IndexedRelations-method coerce,DataFrame,IndexedRelations-method coerce,IndexedRelations,DataFrame-method
+NULL
+
 #' @export
 #' @importFrom S4Vectors Pairs mcols
 makePairsFromIndexedRelations <- function(x) {
@@ -40,4 +56,20 @@ makePairsFromIndexedRelations <- function(x) {
 #' @importFrom S4Vectors first second
 setAs("Pairs", "IndexedRelations", function(from) {
     IndexedRelations(list(first=first(p), second=second(p)))
+})
+
+#' @importClassesFrom S4Vectors DataFrame
+setAs("DataFrame", "IndexedRelations", function(from) {
+    IndexedRelations(from)
+})
+
+#' @importClassesFrom S4Vectors DataFrame
+#' @importFrom S4Vectors DataFrame
+setAs("IndexedRelations", "DataFrame", function(from) {
+    output <- vector("list", ncol(partners(from)))
+    for (i in seq_along(output)) {
+        output[[i]] <- I(partner(from, i))
+    }
+    names(output) <- partnerNames(from)
+    do.call(DataFrame, output)
 })
