fb17049c4b8e0bfe7a3f2ac1b8c988c10e953d97
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-04-14 15:46:22 -0700

    Added tests for the new show method.

---
+++ tests/testthat/test-show.R
@@ -0,0 +1,48 @@
+# This tests the show methods.
+# library(testthat); library(IndexedRelations); source("setup.R"); source("test-show.R")
+
+set.seed(19999)
+r1 <- random_ranges(20)
+r2 <- random_ranges(10)
+r3 <- random_ranges(40)
+
+N <- 100
+i1 <- sample(length(r1), N, replace=TRUE)
+i2 <- sample(length(r2), N, replace=TRUE)
+i3 <- sample(length(r3), N, replace=TRUE)
+
+test_that("show method works correctly with any number of features", {
+    rel <- IndexedRelations(list(i1, i2), list(r1, r2))
+    show(rel)
+
+    rel <- IndexedRelations(list(i1, i2, i3), list(r1, r2, r3))
+    show(rel)
+})
+
+test_that("show method works correctly with named features", {
+    rel <- IndexedRelations(list(A=i1, B=i2, C=i3), list(r1, r2, r3))
+    show(rel)
+
+    featureSetNames(rel) <- c("X", "Y", "Z") # should have no effect.
+    show(rel)
+
+    names(rel) <- sprintf("RELATION_%i", seq_along(rel))
+    show(rel)
+})
+
+test_that("show method works correctly with metadata", {
+    rel <- IndexedRelations(list(A=i1, B=i2, C=i3), list(r1, r2, r3))
+    mcols(rel)$xxx <- runif(length(rel))
+    show(rel)
+
+    mcols(rel)$yyy <- sample(LETTERS, length(rel), replace=TRUE)
+    show(rel)
+
+    metadata(rel)$blah <- "BLAH" # should have no effect.
+    show(rel)
+})
+
+test_that("show method works correctly with empty objects", {
+    rel <- IndexedRelations(list(A=i1, B=i2, C=i3), list(r1, r2, r3))
+    show(rel[0])
+})
